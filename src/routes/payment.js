0cd0b97d4b92e46073ae6fcb0571de5290f0c475
BONGIORNO <bongiorno.leo@gmail.com>
2021-02-01 18:42:32 +0100

    fix issue payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,18 +3,18 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 var express = require("express");
 var router = express.Router();
 
-router.post("/pay", (req, res) => {
+router.post("/pay", async (req, res) => {
   if (!req.user) {
     res.sendStatus(403);
-  }
-  return stripe.charges
-    .create({
+  } else {
+    const result = await stripe.charges.create({
       amount: req.body.amount, // Unit: cents
       currency: "eur",
       source: req.body.tokenId,
       description: "Test payment",
-    })
-    .then((result) => res.status(200).json(result));
+    });
+    res.send(result);
+  }
 });
 
 module.exports = router;
